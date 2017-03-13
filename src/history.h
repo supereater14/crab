0542562a29d12162ef59a83b9b9528c28bc75703
brkurzawa <brkurzaw@mtu.edu>
2017-03-13 13:37:37 -0400

    Removed history.o and updated history.h

--- src/history.h
+++ src/history.h
@@ -1,4 +1,4 @@
-B/*
+/*
 Brett Kurzawa
 CS3141 Team Software Project
 2/13/2017
@@ -10,9 +10,9 @@ Contains the structure for a basic command history. Will be implemented with a d
 #include <string.h>
 
 typedef struct node {
-  char* content;
-  struct node* last;
-  struct node* next;
+    char *content;
+    struct node *last;
+    struct node *next;
 } Node;
 
 // Instance variables
@@ -20,28 +20,40 @@ typedef struct node {
 // Number of elements in the history
 int size;
 // Current position in the history list
-Node* currPos;
+Node *currPos;
 // The first node in the list
-Node* head;
+Node *head;
 // The last node in the list
-Node* tail;
+Node *tail;
+
+// Initialize the empty list
+extern void init();
 
 // Create a new node with content
-extern Node* createNode(Node* last, Node* next, char* content);
+extern Node *createNode(Node *last, Node *next, char *content);
+
 // Adds a new command to the end of the history list
-extern Node* addLast(char* content);
+extern Node *addLast(char *content);
+
 // Returns the first position in the history list
-extern Node* getFirst();
+extern Node *getFirst();
+
 // Returns the last position in the history list
-extern Node* getLast();
+extern Node *getLast();
+
 // Returns the current position in the history list
-extern Node* getCurrPos();
+extern Node *getCurrPos();
+
 // Returns the content of the command in the specified position
-extern char* getContent(Node* pos);
+extern char *getContent(Node *pos);
+
 // Returns the content of the command in the current position
-extern char* getCurrContent();
+extern char *getCurrContent();
+
 // Add a new command to the end of the list
-extern Node* addCommand();
+extern Node *addCommand();
+
 // Getter and Setter for size
 extern int getSize();
+
 extern void setSize();
