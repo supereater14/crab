eb30a700cea77150ed1cff3c00898b4504be819f
brkurzawa <brkurzaw@mtu.edu>
2017-03-13 13:26:34 -0400

    Fixed seg fault. Code should work fine now.

--- src/history.c
+++ src/history.c
@@ -1,70 +1,72 @@
 #include "history.h"
+#include <stdio.h>
 
 // List is empty by default so size is 0
 int size = 0;
 
-Node* head;
-Node* tail;
-Node* currPos;
+Node *head;
+Node *tail;
+Node *currPos;
 
 // Initialize the empty list
 void init() {
-  head = createNode(NULL, NULL, NULL);
-  tail = createNode(head, NULL, NULL);
-  currPos = tail;
+    head = createNode(NULL, NULL, NULL);
+    tail = createNode(head, NULL, NULL);
+    currPos = tail;
 }
 
-Node* createNode(Node* last, Node* next, char* content) {
-  Node* newNode = malloc(sizeof(Node));
-  newNode->content = content;
-  newNode->last = last;
-  newNode->next = next;
-  size++;
-  return newNode;
+Node *createNode(Node *last, Node *next, char *content) {
+    Node *newNode = malloc(sizeof(Node));
+    newNode->content = content;
+    newNode->last = last;
+    newNode->next = next;
+    size++;
+    return newNode;
 }
 
 // Adds a node to the end of the list
-Node* addLast(char* content) {
-  // Add the new node just before the tail
-  Node* newNode = createNode(tail->last, tail, content);
-  return newNode;
+Node *addLast(char *content) {
+    // Add the new node just before the tail
+    Node *newNode = createNode(tail->last, tail, content);
+    return newNode;
 }
 
 // Returns a pointer to the first node in the list
-Node* getFirst() {
-  return first;
+Node *getFirst() {
+    return head->next;
 }
 
 // Returns a pointer to the last node in the list
-Node* getLast() {
-  return last;
+Node *getLast() {
+    return tail->last;
 }
 
 // Returns the content of the command in the specified position
-char* getContent(Node* pos) {
-  return pos->content;
+char *getContent(Node *pos) {
+    return pos->content;
 }
 
 // Returns the content of the command in the current position
-char* getCurrContent() {
-  return getContent(currPos);
+char *getCurrContent() {
+    return getContent(currPos);
 }
 
 // Adds a command to the end of the list
-Node* addCommand(Node* cmd) {
-  addLast(cmd);
+Node *addCommand(char *cmd) {
+    addLast(cmd);
 }
 
 // Return the number of elements in the list
 int getSize() {
-  return size;
+    return size;
 }
 
 // Set the size of the list
 void setSize(int s) {
-  size = s;
+    size = s;
 }
 
-int main(int argc, char** argv) {
-  addLast("test");
+int main(int argc, char **argv) {
+    init();
+    printf("This should print test: %s\n", getContent(addLast("test")));
 }
